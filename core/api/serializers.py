5537a28151cb0dba3f25c0e6ad0c74a58d9d3379
Pirathib Mahalingam <pirathib.m@gmail.com>
2025-06-02 21:11:49 +0200

    edited base info view

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,6 +5,6 @@ from rest_framework import serializers
 
 class BaseInfoSerializer(serializers.Serializer):
     review_count = serializers.IntegerField()
-    average_rating = serializers.IntegerField()
+    average_rating = serializers.DecimalField(decimal_places=1, max_digits=2)
     business_profile_count = serializers.IntegerField()
     offer_count = serializers.IntegerField()
--- core/api/views.py
+++ core/api/views.py
@@ -1,12 +1,13 @@
-
-
+from django.db.models import Avg
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.api.serializers import BaseInfoSerializer
+from offer_app.admin import Offer
 from review_app.models import Review
+from user_auth_app.models import Profile
 
 
 class BaseInfoViewSet(APIView):
@@ -15,10 +16,10 @@ class BaseInfoViewSet(APIView):
     def get(self, request, *args, **kwargs):
         try:
             review_count = Review.objects.all().count()
-            print(review_count)
-            average_rating = "10"
-            business_profile_count = "10"
-            offer_count = "10"
+            average_rating_dict = Review.objects.aggregate(avg_rating=Avg("rating"))
+            average_rating = average_rating_dict["avg_rating"] or 0
+            business_profile_count = Profile.objects.filter(type="business").count()
+            offer_count = Offer.objects.all().count()
             serializer = BaseInfoSerializer({
                 "review_count": review_count,
                 "average_rating": average_rating,
@@ -27,4 +28,4 @@ class BaseInfoViewSet(APIView):
             })
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception:
-            return Response({"error": "Internal server was occured"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"error": "Internal server error occured"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
